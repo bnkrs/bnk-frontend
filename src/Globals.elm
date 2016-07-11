7358206c6ec44e039990f77ce0551cf1ec68efea
Julian Dax <julian.dax@uni-siegen.de>
2016-07-11 16:29:01 +0200

    Add JSON ecoding functions to Globals

--- src/Globals.elm
+++ src/Globals.elm
@@ -1,9 +1,23 @@
 module Globals exposing (..)
+import Json.Encode as JE
+
 
 type alias Model =
   { apiToken : String
   , username : String
   }
 
+
 initialModel : Model
 initialModel = { apiToken = "",  username = "" }
+
+
+tokenJson : Model -> JE.Value
+tokenJson model =
+  JE.object
+  [ ("token", JE.string model.apiToken) ]
+
+
+tokenJsonString : Model -> String
+tokenJsonString model =
+  JE.encode 0 <| tokenJson model
