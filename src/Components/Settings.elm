c6923a96b3ff8b4a3d8443dbabe6ec70bf23a6e1
Julian Dax <julian.dax@uni-siegen.de>
2016-07-13 14:21:13 +0200

    Add settings view

--- src/Components/Settings.elm
+++ src/Components/Settings.elm
@@ -1,6 +1,6 @@
 module Components.Settings exposing (..)
 import Html exposing (..)
--- import Html.Events exposing (onInput, onFocus, onClick)
+import Html.Events exposing (onClick, onCheck, onInput)
 import Html.Attributes exposing (..)
 import Json.Decode as JD exposing ((:=))
 -- import Json.Encode as JE
@@ -8,6 +8,7 @@ import HttpBuilder exposing (..)
 import Maybe
 import Task
 import Navigation
+import Regex
 
 -- import Utils.HttpUtils exposing (httpErrorToString)
 import Globals
@@ -66,6 +67,13 @@ urlChange model =
 
 type Msg = GetSettingsFailed (Error String)
   | GetSettingSuccessful (Response GetSettingsResponse)
+  | Save
+  | ToggleTransacionLogging Bool
+  | ActivateTransactionLogging
+  | DeactivateTransactionLogging
+  | SetRecoveryMethodEmail
+  | SetRecoveryMethodPhrase
+  | UpdateEmail String
 
 
 type alias UpdateResult =
@@ -97,6 +105,47 @@ update msg model global =
         , globals = global
         , cmd = Cmd.none
         }
+    Save ->
+      { model = model, globals = global, cmd = Cmd.none }
+    ToggleTransacionLogging enabled ->
+      { model =
+        { model
+        | transactionLogging = transactionLoggingFromBool enabled
+        },
+        globals = global, cmd = Cmd.none
+      }
+    ActivateTransactionLogging ->
+      {
+        model = { model | transactionLogging = Enabled }
+      , globals = global
+      , cmd = Cmd.none
+      }
+    DeactivateTransactionLogging ->
+      {
+        model = { model | transactionLogging = Disabled }
+      , globals = global
+      , cmd = Cmd.none
+      }
+    SetRecoveryMethodEmail ->
+      {
+        model = { model | recoveryMethod = EMail }
+      , globals = global
+      , cmd = Cmd.none
+      }
+    SetRecoveryMethodPhrase ->
+      {
+        model = { model | recoveryMethod = Phrase }
+      , globals = global
+      , cmd = Cmd.none
+      }
+    UpdateEmail mail ->
+      {
+        model = { model | email = mail }
+      , globals = global
+      , cmd = Cmd.none
+      }
+
+
 
 
 getSettings : Globals.Model -> Cmd Msg
@@ -132,7 +181,97 @@ view model =
   div [ class "row" ][
     div [ class "col-xs-3" ] [],
     div [ class "col-xs-6" ]
-      [ text "Register view"
-      ],
+      [ formView model ],
     div [ class "col-xs-3" ] [ ]
   ]
+
+formView : Model -> Html Msg
+formView model =
+  div [class "panel panel-primary login-form"]
+    [
+      div [class "panel-heading"] [text "Settings"],
+      div [class "panel-body"]
+        [ div [ class <| "form-group"]
+            [
+              transactionLoggingCheckbox model
+            , recoveryMethodDropdown model
+            , if model.recoveryMethod == EMail then emailField model else text ""
+            ]
+
+        , button [ class "btn btn-primary", type' "submit"
+          , onClick Save
+          , disabled <| not <| modelValid model ]
+            [ text "Save" ]
+        ]
+    ]
+
+transactionLoggingCheckbox : Model -> Html Msg
+transactionLoggingCheckbox model =
+  let
+    activateButtonClass= if model.transactionLogging == Enabled then "active" else ""
+    deactivateButtonClass = if model.transactionLogging == Disabled then "active" else ""
+  in
+    div [ class <| "form-group"]
+        [ label [ for "userName" ][ text "Transaction Logging" ]
+        , div []
+          [
+              div [ class "btn-group", attribute "role" "group" ]
+              [ button [ class <| "btn btn-default " ++ deactivateButtonClass
+                  , type' "button"
+                  , onClick DeactivateTransactionLogging ]
+                  [ text "Record all transactions on server" ]
+              , button [ class <| "btn btn-default " ++  activateButtonClass
+                  , type' "button"
+                  , onClick ActivateTransactionLogging ]
+                  [ text "Do not record transactions" ]
+              ]
+          ]
+        ]
+
+recoveryMethodDropdown : Model -> Html Msg
+recoveryMethodDropdown model =
+  let
+    emailButtonClass= if model.recoveryMethod == EMail then "active" else ""
+    phraseButtonClass = if model.recoveryMethod == Phrase then "active" else ""
+  in
+    div [ class <| "form-group"]
+        [ label [ for "userName" ]
+            [ text "Recovery Method" ]
+        , div []
+          [
+              div [ class "btn-group", attribute "role" "group" ]
+              [ button [ class <| "btn btn-default " ++ emailButtonClass
+                  , type' "button"
+                  , onClick SetRecoveryMethodEmail ]
+                  [ text "E-Mail" ]
+              , button [ class <| "btn btn-default " ++  phraseButtonClass
+                  , type' "button"
+                  , onClick SetRecoveryMethodPhrase ]
+                  [ text "Recovery Phrase" ]
+              ]
+          ]
+        ]
+
+emailField : Model -> Html Msg
+emailField model =
+  div [ class <| "form-group"]
+      [ label [ for "userName" ]
+          [ text "E-Mail" ]
+      , input [ class "form-control"
+        , placeholder "user@domain.tld"
+        , type' "text"
+        , value model.email
+        , onInput UpdateEmail ]
+          []
+      ]
+
+modelValid : Model -> Bool
+modelValid model =
+  model.recoveryMethod == Phrase || emailValid model
+
+emailValid : Model -> Bool
+emailValid model =
+  let
+    emailRegex = Regex.caseInsensitive (Regex.regex "^\\S+@\\S+\\.\\S+$")
+  in
+    (Regex.contains emailRegex model.email)
